55098539b5ef1bfb74696f5949f434e628b239f9
vassik <anatoly.vasilevskiy@sintef.no>
2017-11-01 10:05:38 +0100

    script to download and update tool
    Conflicts:
    	Jenkinsfile

---
+++ download_sut.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SUT_URL="https://github.com/vassik/bvr-diversity.git"
+SUT_NAME="bvr-diversity"
+
+if [[ -d $SUT_NAME ]]; then
+	echo "sut has been already downloaded, cleaning, updating..."
+	cd $SUT_NAME
+	git reset --hard
+	git clean -fd
+	git pull
+else
+	echo "sut has not beed downloaded..."
+	git clone $SUT_URL
+fi
