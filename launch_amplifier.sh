881a8234f06c0582b75cc24b942d66bc08d8c6e1
vassik <anatoly.vasilevskiy@sintef.no>
2017-11-27 12:54:16 +0100

    calculating directory for stamp amplifier on the host, since we execute in the container and all evaluatin is done inside container, but the container is run outside

--- launch_amplifier.sh
+++ launch_amplifier.sh
@@ -2,5 +2,9 @@
 
 echo "Launching amplifier!"
 cd stamp-samples/xwiki-mono
-echo $(pwd)
-docker run -v $(pwd):/root/workingdir songhui/stamp-config-amp
+HOST_JENKINS_HOME="/home/sintef/config-tool/jenkins_volumes/jenkins_home/"
+CONT_JENKINS_HOME="/var/jenkins_home/"
+CPWD=$(pwd)
+HOST_STAMP_DIR="${CPWD/$CONT_JENKINS_HOME/$HOST_JENKINS_HOME}"
+echo "Calculated host directory ${HOST_STAMP_DIR}"
+docker run -v ${HOST_STAMP_DIR}:/:root/workingdir songhui/stamp-config-amp
