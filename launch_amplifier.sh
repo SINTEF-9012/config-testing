e5d8dee368b82e5341105d9a0543d9856f104cde
vassik <anatoly.vasilevskiy@sintef.no>
2017-11-27 12:57:48 +0100

    oops typo

--- launch_amplifier.sh
+++ launch_amplifier.sh
@@ -7,4 +7,4 @@ CONT_JENKINS_HOME="/var/jenkins_home/"
 CPWD=$(pwd)
 HOST_STAMP_DIR="${CPWD/$CONT_JENKINS_HOME/$HOST_JENKINS_HOME}"
 echo "Calculated host directory ${HOST_STAMP_DIR}"
-docker run -v ${HOST_STAMP_DIR}:/:root/workingdir songhui/stamp-config-amp
+docker run -v ${HOST_STAMP_DIR}:root/workingdir songhui/stamp-config-amp
