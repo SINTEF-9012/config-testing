96c9fc8c376133c550350d51d12cbae4b6b8d2bb
vassik <anatoly.vasilevskiy@sintef.no>
2017-11-27 13:23:56 +0100

    do not mind the last commit, there was just a typo

--- launch_amplifier.sh
+++ launch_amplifier.sh
@@ -2,12 +2,9 @@
 
 echo "Launching amplifier!"
 cd stamp-samples/xwiki-mono
-#HOST_JENKINS_HOME="/home/sintef/config-tool/jenkins_volumes/jenkins_home/"
-#CONT_JENKINS_HOME="/var/jenkins_home/"
-#CPWD=$(pwd)
-#HOST_STAMP_DIR="${CPWD/$CONT_JENKINS_HOME/$HOST_JENKINS_HOME}"
-#echo "Calculated host directory ${HOST_STAMP_DIR}"
-#pwd is actually evalutated in the container, but the container is run on the host, thus there is mismatch for paths
-#created a symbolin link to the host to point(/var/jenkins_home) to /home/sintef/config-tool/jenkins_volumes/jenkins_home/
-#urgly urgly!!!
-docker run -v $(pwd):root/workingdir songhui/stamp-config-amp
+HOST_JENKINS_HOME="/home/sintef/config-tool/jenkins_volumes/jenkins_home/"
+CONT_JENKINS_HOME="/var/jenkins_home/"
+CPWD=$(pwd)
+HOST_STAMP_DIR="${CPWD/$CONT_JENKINS_HOME/$HOST_JENKINS_HOME}"
+echo "Calculated host directory ${HOST_STAMP_DIR}"
+docker run -v ${HOST_STAMP_DIR}:/root/workingdir songhui/stamp-config-amp
